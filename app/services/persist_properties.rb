616b98f2ca05bc327f18fbec072a665ebf66143e
Stefan Yakimov <stefan.yakimov@receipt-bank.com>
2020-04-10 21:09:55 +0300

    Reduce the amount of created PriceChanges

--- app/services/persist_properties.rb
+++ app/services/persist_properties.rb
@@ -17,10 +17,6 @@ module PersistProperties
                                     location: imot.location)
       elsif property.current_price != imot.price
         property.update(change_in_price: imot.price - property.initial_price, current_price: imot.price)
-      end
-
-      last_recorded_change = property.price_changes.order(:id).last
-      if last_recorded_change.nil? || last_recorded_change.updated_price != property.current_price
         PriceChange.create! property: property, updated_price: property.current_price
       end
     end
