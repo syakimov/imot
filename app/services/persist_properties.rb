9fa80f73838dabea42f7c1b85870c1d09b7edfc3
Stefan Yakimov <stefan.yakimov@receipt-bank.com>
2020-04-14 10:12:03 +0300

    Update change in price as well

--- app/services/persist_properties.rb
+++ app/services/persist_properties.rb
@@ -1,22 +1,22 @@
 module PersistProperties
   class << self
-    def execute(imots)
-      imots.each do |imot|
-        save_property imot
+    def execute(fetched_data_collection)
+      fetched_data_collection.each do |fetched_property_data|
+        save_property fetched_property_data
       end
     end
 
-    def save_property(imot)
-      property = Property.find_by remote_id: imot.remote_id
+    def save_property(fetched_property_data)
+      property = Property.find_by remote_id: fetched_property_data.remote_id
 
       if property.nil?
-        property = Property.create!(remote_id: imot.remote_id,
-                                    current_price: imot.price,
-                                    description: imot.description,
+        property = Property.create!(remote_id: fetched_property_data.remote_id,
+                                    current_price: fetched_property_data.price,
+                                    description: fetched_property_data.description,
                                     change_in_price: 0,
-                                    location: imot.location)
-      elsif imot.price && property.current_price != imot.price
-        property.update(change_in_price: imot.price - property.initial_price, current_price: imot.price)
+                                    location: fetched_property_data.location)
+      elsif fetched_property_data.price && property.current_price != fetched_property_data.price
+        property.update(change_in_price: fetched_property_data.price - property.initial_price, current_price: fetched_property_data.price)
         PriceChange.create! property: property, updated_price: property.current_price
       end
     rescue
@@ -37,7 +37,10 @@ module PersistProperties
 
     def update_property(fetched_property_data, property)
       update_hash = {}
-      update_hash[:current_price] = fetched_property_data.price if fetched_property_data.price
+      if fetched_property_data.price && property.current_price != fetched_property_data.price
+        update_hash[:current_price] = fetched_property_data.price
+        update_hash[:change_in_price] = fetched_property_data.price - property.initial_price
+      end
       update_hash[:description] = fetched_property_data.description if fetched_property_data.description
       update_hash[:detailed_description] = fetched_property_data.detailed_description if fetched_property_data.detailed_description
 
